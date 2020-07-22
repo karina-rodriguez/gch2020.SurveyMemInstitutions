501294c34cc35c83216c9cb66f63e62010cdd5a3
karina.r.e <k.rodriguez@brighton.ac.uk>
2020-07-22 16:55:23 +0100

    first commit

---
+++ EGauthorGuidelines-GCH2020-sub.d
@@ -0,0 +1,283 @@
+#===Dependents, and related info, for EGauthorGuidelines-GCH2020-sub:
+EGauthorGuidelines-GCH2020-sub.pdf :\
+    ./eg-alpha-doi.bst\
+    /opt/local/etc/texmf/texmf.cnf\
+    /opt/local/share/texmf-texlive/fonts/enc/dvips/base/8r.enc\
+    /opt/local/share/texmf-texlive/fonts/map/fontname/texfonts.map\
+    /opt/local/share/texmf-texlive/fonts/tfm/adobe/courier/pcrr8t.tfm\
+    /opt/local/share/texmf-texlive/fonts/tfm/adobe/times/ptmb7t.tfm\
+    /opt/local/share/texmf-texlive/fonts/tfm/adobe/times/ptmb8r.tfm\
+    /opt/local/share/texmf-texlive/fonts/tfm/adobe/times/ptmb8t.tfm\
+    /opt/local/share/texmf-texlive/fonts/tfm/adobe/times/ptmbi8r.tfm\
+    /opt/local/share/texmf-texlive/fonts/tfm/adobe/times/ptmbi8t.tfm\
+    /opt/local/share/texmf-texlive/fonts/tfm/adobe/times/ptmr8r.tfm\
+    /opt/local/share/texmf-texlive/fonts/tfm/adobe/times/ptmr8t.tfm\
+    /opt/local/share/texmf-texlive/fonts/tfm/adobe/times/ptmrc8t.tfm\
+    /opt/local/share/texmf-texlive/fonts/tfm/adobe/times/ptmri7t.tfm\
+    /opt/local/share/texmf-texlive/fonts/tfm/adobe/times/ptmri8r.tfm\
+    /opt/local/share/texmf-texlive/fonts/tfm/adobe/times/ptmri8t.tfm\
+    /opt/local/share/texmf-texlive/fonts/tfm/adobe/times/zpsycmrv.tfm\
+    /opt/local/share/texmf-texlive/fonts/tfm/adobe/times/zptmcmr.tfm\
+    /opt/local/share/texmf-texlive/fonts/tfm/adobe/times/zptmcmrm.tfm\
+    /opt/local/share/texmf-texlive/fonts/tfm/jknappen/ec/ecrm0900.tfm\
+    /opt/local/share/texmf-texlive/fonts/tfm/public/cm/cmr9.tfm\
+    /opt/local/share/texmf-texlive/fonts/tfm/public/cm/cmsy6.tfm\
+    /opt/local/share/texmf-texlive/fonts/tfm/public/cm/cmsy8.tfm\
+    /opt/local/share/texmf-texlive/fonts/tfm/public/cm/cmsy9.tfm\
+    /opt/local/share/texmf-texlive/fonts/tfm/public/pslatex/pcrr8rn.tfm\
+    /opt/local/share/texmf-texlive/fonts/tfm/public/pslatex/pcrr8tn.tfm\
+    /opt/local/share/texmf-texlive/fonts/type1/public/amsfonts/cm/cmsy9.pfb\
+    /opt/local/share/texmf-texlive/fonts/type1/urw/courier/ucrr8a.pfb\
+    /opt/local/share/texmf-texlive/fonts/type1/urw/times/utmb8a.pfb\
+    /opt/local/share/texmf-texlive/fonts/type1/urw/times/utmbi8a.pfb\
+    /opt/local/share/texmf-texlive/fonts/type1/urw/times/utmr8a.pfb\
+    /opt/local/share/texmf-texlive/fonts/type1/urw/times/utmri8a.pfb\
+    /opt/local/share/texmf-texlive/fonts/vf/adobe/times/ptmb8t.vf\
+    /opt/local/share/texmf-texlive/fonts/vf/adobe/times/ptmbi8t.vf\
+    /opt/local/share/texmf-texlive/fonts/vf/adobe/times/ptmr8t.vf\
+    /opt/local/share/texmf-texlive/fonts/vf/adobe/times/ptmrc8t.vf\
+    /opt/local/share/texmf-texlive/fonts/vf/adobe/times/ptmri8t.vf\
+    /opt/local/share/texmf-texlive/fonts/vf/public/pslatex/pcrr8tn.vf\
+    /opt/local/share/texmf-texlive/tex/context/base/mkii/supp-pdf.mkii\
+    /opt/local/share/texmf-texlive/tex/generic/ifxetex/ifxetex.sty\
+    /opt/local/share/texmf-texlive/tex/generic/oberdiek/gettitlestring.sty\
+    /opt/local/share/texmf-texlive/tex/generic/oberdiek/hobsub-generic.sty\
+    /opt/local/share/texmf-texlive/tex/generic/oberdiek/hobsub-hyperref.sty\
+    /opt/local/share/texmf-texlive/tex/generic/oberdiek/ifpdf.sty\
+    /opt/local/share/texmf-texlive/tex/latex/base/fontenc.sty\
+    /opt/local/share/texmf-texlive/tex/latex/base/t1enc.def\
+    /opt/local/share/texmf-texlive/tex/latex/base/t1enc.sty\
+    /opt/local/share/texmf-texlive/tex/latex/cite/cite.sty\
+    /opt/local/share/texmf-texlive/tex/latex/comment/comment.sty\
+    /opt/local/share/texmf-texlive/tex/latex/graphics-cfg/color.cfg\
+    /opt/local/share/texmf-texlive/tex/latex/graphics-cfg/graphics.cfg\
+    /opt/local/share/texmf-texlive/tex/latex/graphics-def/pdftex.def\
+    /opt/local/share/texmf-texlive/tex/latex/graphics/color.sty\
+    /opt/local/share/texmf-texlive/tex/latex/graphics/graphics.sty\
+    /opt/local/share/texmf-texlive/tex/latex/graphics/graphicx.sty\
+    /opt/local/share/texmf-texlive/tex/latex/graphics/keyval.sty\
+    /opt/local/share/texmf-texlive/tex/latex/graphics/trig.sty\
+    /opt/local/share/texmf-texlive/tex/latex/hyperref/backref.sty\
+    /opt/local/share/texmf-texlive/tex/latex/hyperref/hpdftex.def\
+    /opt/local/share/texmf-texlive/tex/latex/hyperref/hyperref.sty\
+    /opt/local/share/texmf-texlive/tex/latex/hyperref/nameref.sty\
+    /opt/local/share/texmf-texlive/tex/latex/hyperref/pd1enc.def\
+    /opt/local/share/texmf-texlive/tex/latex/latexconfig/epstopdf-sys.cfg\
+    /opt/local/share/texmf-texlive/tex/latex/latexconfig/hyperref.cfg\
+    /opt/local/share/texmf-texlive/tex/latex/multirow/multirow.sty\
+    /opt/local/share/texmf-texlive/tex/latex/oberdiek/auxhook.sty\
+    /opt/local/share/texmf-texlive/tex/latex/oberdiek/epstopdf-base.sty\
+    /opt/local/share/texmf-texlive/tex/latex/oberdiek/grfext.sty\
+    /opt/local/share/texmf-texlive/tex/latex/oberdiek/kvoptions.sty\
+    /opt/local/share/texmf-texlive/tex/latex/oberdiek/rerunfilecheck.sty\
+    /opt/local/share/texmf-texlive/tex/latex/psnfss/omlptmcm.fd\
+    /opt/local/share/texmf-texlive/tex/latex/psnfss/omsptm.fd\
+    /opt/local/share/texmf-texlive/tex/latex/psnfss/omxpsycm.fd\
+    /opt/local/share/texmf-texlive/tex/latex/psnfss/ot1ptm.fd\
+    /opt/local/share/texmf-texlive/tex/latex/psnfss/ot1ptmcm.fd\
+    /opt/local/share/texmf-texlive/tex/latex/url/url.sty\
+    /opt/local/var/db/texmf/fonts/map/pdftex/updmap/pdftex.map\
+    /opt/local/var/db/texmf/web2c/pdftex/pdflatex.fmt\
+    EGauthorGuidelines-GCH2020-sub.tex\
+    dfT1pcr.fd\
+    dfT1phv.fd\
+    dfT1ptm.fd\
+    dfadobe.sty\
+    egbibsample.bib\
+    egpubl.cls\
+    egweblnk.sty\
+    gch2020.sty\
+    images/audiencesUK.png\
+    images/audiencesUS.png\
+    images/cloud.png\
+    images/museumtype.png\
+    images/numdigitalofferings.png
+#===End dependents for EGauthorGuidelines-GCH2020-sub:
+
+#===Phony rules for EGauthorGuidelines-GCH2020-sub:
+
+./eg-alpha-doi.bst :
+
+/opt/local/etc/texmf/texmf.cnf :
+
+/opt/local/share/texmf-texlive/fonts/enc/dvips/base/8r.enc :
+
+/opt/local/share/texmf-texlive/fonts/map/fontname/texfonts.map :
+
+/opt/local/share/texmf-texlive/fonts/tfm/adobe/courier/pcrr8t.tfm :
+
+/opt/local/share/texmf-texlive/fonts/tfm/adobe/times/ptmb7t.tfm :
+
+/opt/local/share/texmf-texlive/fonts/tfm/adobe/times/ptmb8r.tfm :
+
+/opt/local/share/texmf-texlive/fonts/tfm/adobe/times/ptmb8t.tfm :
+
+/opt/local/share/texmf-texlive/fonts/tfm/adobe/times/ptmbi8r.tfm :
+
+/opt/local/share/texmf-texlive/fonts/tfm/adobe/times/ptmbi8t.tfm :
+
+/opt/local/share/texmf-texlive/fonts/tfm/adobe/times/ptmr8r.tfm :
+
+/opt/local/share/texmf-texlive/fonts/tfm/adobe/times/ptmr8t.tfm :
+
+/opt/local/share/texmf-texlive/fonts/tfm/adobe/times/ptmrc8t.tfm :
+
+/opt/local/share/texmf-texlive/fonts/tfm/adobe/times/ptmri7t.tfm :
+
+/opt/local/share/texmf-texlive/fonts/tfm/adobe/times/ptmri8r.tfm :
+
+/opt/local/share/texmf-texlive/fonts/tfm/adobe/times/ptmri8t.tfm :
+
+/opt/local/share/texmf-texlive/fonts/tfm/adobe/times/zpsycmrv.tfm :
+
+/opt/local/share/texmf-texlive/fonts/tfm/adobe/times/zptmcmr.tfm :
+
+/opt/local/share/texmf-texlive/fonts/tfm/adobe/times/zptmcmrm.tfm :
+
+/opt/local/share/texmf-texlive/fonts/tfm/jknappen/ec/ecrm0900.tfm :
+
+/opt/local/share/texmf-texlive/fonts/tfm/public/cm/cmr9.tfm :
+
+/opt/local/share/texmf-texlive/fonts/tfm/public/cm/cmsy6.tfm :
+
+/opt/local/share/texmf-texlive/fonts/tfm/public/cm/cmsy8.tfm :
+
+/opt/local/share/texmf-texlive/fonts/tfm/public/cm/cmsy9.tfm :
+
+/opt/local/share/texmf-texlive/fonts/tfm/public/pslatex/pcrr8rn.tfm :
+
+/opt/local/share/texmf-texlive/fonts/tfm/public/pslatex/pcrr8tn.tfm :
+
+/opt/local/share/texmf-texlive/fonts/type1/public/amsfonts/cm/cmsy9.pfb :
+
+/opt/local/share/texmf-texlive/fonts/type1/urw/courier/ucrr8a.pfb :
+
+/opt/local/share/texmf-texlive/fonts/type1/urw/times/utmb8a.pfb :
+
+/opt/local/share/texmf-texlive/fonts/type1/urw/times/utmbi8a.pfb :
+
+/opt/local/share/texmf-texlive/fonts/type1/urw/times/utmr8a.pfb :
+
+/opt/local/share/texmf-texlive/fonts/type1/urw/times/utmri8a.pfb :
+
+/opt/local/share/texmf-texlive/fonts/vf/adobe/times/ptmb8t.vf :
+
+/opt/local/share/texmf-texlive/fonts/vf/adobe/times/ptmbi8t.vf :
+
+/opt/local/share/texmf-texlive/fonts/vf/adobe/times/ptmr8t.vf :
+
+/opt/local/share/texmf-texlive/fonts/vf/adobe/times/ptmrc8t.vf :
+
+/opt/local/share/texmf-texlive/fonts/vf/adobe/times/ptmri8t.vf :
+
+/opt/local/share/texmf-texlive/fonts/vf/public/pslatex/pcrr8tn.vf :
+
+/opt/local/share/texmf-texlive/tex/context/base/mkii/supp-pdf.mkii :
+
+/opt/local/share/texmf-texlive/tex/generic/ifxetex/ifxetex.sty :
+
+/opt/local/share/texmf-texlive/tex/generic/oberdiek/gettitlestring.sty :
+
+/opt/local/share/texmf-texlive/tex/generic/oberdiek/hobsub-generic.sty :
+
+/opt/local/share/texmf-texlive/tex/generic/oberdiek/hobsub-hyperref.sty :
+
+/opt/local/share/texmf-texlive/tex/generic/oberdiek/ifpdf.sty :
+
+/opt/local/share/texmf-texlive/tex/latex/base/fontenc.sty :
+
+/opt/local/share/texmf-texlive/tex/latex/base/t1enc.def :
+
+/opt/local/share/texmf-texlive/tex/latex/base/t1enc.sty :
+
+/opt/local/share/texmf-texlive/tex/latex/cite/cite.sty :
+
+/opt/local/share/texmf-texlive/tex/latex/comment/comment.sty :
+
+/opt/local/share/texmf-texlive/tex/latex/graphics-cfg/color.cfg :
+
+/opt/local/share/texmf-texlive/tex/latex/graphics-cfg/graphics.cfg :
+
+/opt/local/share/texmf-texlive/tex/latex/graphics-def/pdftex.def :
+
+/opt/local/share/texmf-texlive/tex/latex/graphics/color.sty :
+
+/opt/local/share/texmf-texlive/tex/latex/graphics/graphics.sty :
+
+/opt/local/share/texmf-texlive/tex/latex/graphics/graphicx.sty :
+
+/opt/local/share/texmf-texlive/tex/latex/graphics/keyval.sty :
+
+/opt/local/share/texmf-texlive/tex/latex/graphics/trig.sty :
+
+/opt/local/share/texmf-texlive/tex/latex/hyperref/backref.sty :
+
+/opt/local/share/texmf-texlive/tex/latex/hyperref/hpdftex.def :
+
+/opt/local/share/texmf-texlive/tex/latex/hyperref/hyperref.sty :
+
+/opt/local/share/texmf-texlive/tex/latex/hyperref/nameref.sty :
+
+/opt/local/share/texmf-texlive/tex/latex/hyperref/pd1enc.def :
+
+/opt/local/share/texmf-texlive/tex/latex/latexconfig/epstopdf-sys.cfg :
+
+/opt/local/share/texmf-texlive/tex/latex/latexconfig/hyperref.cfg :
+
+/opt/local/share/texmf-texlive/tex/latex/multirow/multirow.sty :
+
+/opt/local/share/texmf-texlive/tex/latex/oberdiek/auxhook.sty :
+
+/opt/local/share/texmf-texlive/tex/latex/oberdiek/epstopdf-base.sty :
+
+/opt/local/share/texmf-texlive/tex/latex/oberdiek/grfext.sty :
+
+/opt/local/share/texmf-texlive/tex/latex/oberdiek/kvoptions.sty :
+
+/opt/local/share/texmf-texlive/tex/latex/oberdiek/rerunfilecheck.sty :
+
+/opt/local/share/texmf-texlive/tex/latex/psnfss/omlptmcm.fd :
+
+/opt/local/share/texmf-texlive/tex/latex/psnfss/omsptm.fd :
+
+/opt/local/share/texmf-texlive/tex/latex/psnfss/omxpsycm.fd :
+
+/opt/local/share/texmf-texlive/tex/latex/psnfss/ot1ptm.fd :
+
+/opt/local/share/texmf-texlive/tex/latex/psnfss/ot1ptmcm.fd :
+
+/opt/local/share/texmf-texlive/tex/latex/url/url.sty :
+
+/opt/local/var/db/texmf/fonts/map/pdftex/updmap/pdftex.map :
+
+/opt/local/var/db/texmf/web2c/pdftex/pdflatex.fmt :
+
+EGauthorGuidelines-GCH2020-sub.tex :
+
+dfT1pcr.fd :
+
+dfT1phv.fd :
+
+dfT1ptm.fd :
+
+dfadobe.sty :
+
+egbibsample.bib :
+
+egpubl.cls :
+
+egweblnk.sty :
+
+gch2020.sty :
+
+images/audiencesUK.png :
+
+images/audiencesUS.png :
+
+images/cloud.png :
+
+images/museumtype.png :
+
+images/numdigitalofferings.png :
+
+#===End phony rules for EGauthorGuidelines-GCH2020-sub:
